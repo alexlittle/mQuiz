05d32bc10d91835a6ee6dedec7527a0c96cf7531
Alex Little <alex@alexlittle.net>
2012-10-16 17:09:53 +0200

    add other rewrite urls

--- api/module.php
+++ api/module.php
@@ -2,8 +2,13 @@
 include_once('../config.php');
 $modid = optional_param("modid",0,PARAM_INT);
 $lang = optional_param("lang","en",PARAM_TEXT);
+
 $module = $API->getModule($modid, $lang);
-if($module){
+
+if($module instanceof Module){
 	echo json_encode($module);
+} else {
+	$modules = $API->getModules($lang);
+	echo json_encode($modules);
 }
 ?>
